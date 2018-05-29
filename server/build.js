6d7e1b6bc1d07c25c6c39941c709b4b2ed8a8288
aaron <aahuang@berkeley.edu>
2018-05-29 15:03:56 -0700

    streamlined pages (and git bash hopefully)

---
+++ server/build.js
@@ -0,0 +1,4 @@
+//use this file to sync package.json in this directory and the one above it
+//keep ALL lines the same except `npm start`, which becomes `"start": "node ./server/bin/www"`
+
+//until this utility is finished, manually sync the two files for now
